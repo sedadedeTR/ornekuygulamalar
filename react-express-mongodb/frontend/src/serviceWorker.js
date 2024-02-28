434b9bcb43fc451d79b40fd5a099994c1da05e93
Seda DEDE <sedadede.tr@gmail.com>
2024-02-29 01:26:19 +0300

    test1

--- react-express-mongodb/frontend/src/serviceWorker.js
+++ react-express-mongodb/frontend/src/serviceWorker.js
@@ -34,7 +34,7 @@ export function register(config) {
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
-      if (isLocalhost) {
+      //if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
 
@@ -46,10 +46,10 @@ export function register(config) {
               'worker. To learn more, visit https://bit.ly/CRA-PWA'
           );
         });
-      } else {
+      //} else {
         // Is not localhost. Just register service worker
-        registerValidSW(swUrl, config);
-      }
+      //  registerValidSW(swUrl, config);
+      //}
     });
   }
 }
